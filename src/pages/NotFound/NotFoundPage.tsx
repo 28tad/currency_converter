811a14c0d3d972bb70f6a35d9419d93606acdef9
28tad <alimaxcooll@gmail.com>
2024-11-26 16:11:31 +0300

    update Not Found Page: add button for navigation

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,3 +1,11 @@
-export function NotFoundPage() {
-  return <div>NotFoundPage</div>;
-}
+import { Link } from 'react-router-dom';
+import { RoutePath } from '@/app/router/routeConfig';
+import cls from './NotFoundPage.module.scss';
+
+export const NotFoundPage: React.FC = () => (
+  <div className={cls.notFoundPage}>
+    <h1>404</h1>
+    <p>Страница не найдена</p>
+    <Link to={RoutePath.rates}>Вернуться на главную</Link>
+  </div>
+);
